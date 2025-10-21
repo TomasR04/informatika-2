7e6e0640738290d19c7e92ee420d59fb83edfcc4
tomasracil <tomas.racil@unob.cz>
2025-10-21 13:36:45 +0000

    priklad z hodiny 21.10. fix

--- priklady-z-hodin/2025-2026/nejvetsi-nejmensi/main.cpp
+++ priklady-z-hodin/2025-2026/nejvetsi-nejmensi/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 void najdi_nejmensi_nejvetsi(int* pole, int velikost_pole, 
-    int* nejvetsi, int* nejmensi){
-        while(true){
+    int** nejvetsi, int** nejmensi){
+        int* stop = pole+velikost_pole;
+        *nejvetsi = pole;
+        *nejmensi = pole;
+        while(!(pole==stop)){
             std::cout<< "adresa: "<<pole
             << ", hodnota: "<< *pole <<std::endl;
+            if(**nejmensi>*pole){
+                *nejmensi=pole;
+            }
+            if(**nejvetsi<*pole){
+                *nejvetsi=pole;
+            }
             pole++;
         }
 
@@ -13,7 +22,7 @@ int main(){
     int pole[10]={9,4,6,27,9,2,3,7,5,8};
     int* nejmensi;
     int* nejvetsi;
-    najdi_nejmensi_nejvetsi(pole, 10, nejvetsi, nejmensi);
+    najdi_nejmensi_nejvetsi(pole, 10, &nejvetsi, &nejmensi);
 
     std::cout<<*nejmensi << ", " << *nejvetsi <<std::endl;
 
