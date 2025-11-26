9b43d41deb71a2fc4f1178bd2191d4e66ea15203
tomasracil <tomas.racil@unob.cz>
2025-11-26 13:52:19 +0000

    Add implementation for printing a tree structure based on user-defined height

---
+++ priklady-z-hodin/2025-2026/prvni-priklad-python/main.py
@@ -0,0 +1,8 @@
+vyska:int = int(input("Zadej vysku stromu: "));
+
+for i in range(vyska):
+    # radek:str = ""
+    # for j in range(i+1):
+    #     radek+="*"
+    # print(radek)
+    print("*"*(i+1))
