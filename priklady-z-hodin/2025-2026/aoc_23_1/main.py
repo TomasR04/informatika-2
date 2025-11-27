83e82dddbb49dcfeb2ecf3dd265a18c8eb5594d3
tomasracil <tomas.racil@unob.cz>
2025-11-27 10:11:13 +0000

    Přiklady z hodiny 27.11.2025

--- priklady-z-hodin/2025-2026/aoc_23_1/main.py
+++ priklady-z-hodin/2025-2026/aoc_23_1/main.py
@@ -1,15 +1,21 @@
 from os.path import join, realpath, dirname
 
-cesta = join(dirname(realpath(__file__)),"data.txt")
+numbers = ("one", "two", "three", "four", "five",
+           "six", "seven", "eight", "nine")
 
-sum = 0
+cesta = join(dirname(realpath(__file__)), "data.txt")
 
-with open(cesta, "r", encoding="utf-8") as f:  
-    for c, radek in enumerate(f):
-        # print(c,line)
-        cisla = [znak for znak in radek if znak.isdigit()]
-        sum+=int(cisla[0]+cisla[-1])
-        # for znak in radek:
-        #     if znak.isdigit(): print(znak, end=", ")
+suma = 0
+
+with open(cesta, "r", encoding="utf-8") as f:
+    for radek in f:
+        cisla = [
+            int(znak) if znak.isdigit() else
+            [idx+1 
+             for idx, number in enumerate(numbers)
+             if radek[i:].startswith(number)][0]
+            for i, znak in enumerate(radek)
+            if radek.startswith(numbers, i) or znak.isdigit()]
+        suma += cisla[0]*10+cisla[-1]
         print(cisla)
-print(sum)
+print(suma)
