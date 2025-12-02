93d4994477a1f4b1a4de3395c3c44aacecf8fd64
tomasracil <tomas.racil@unob.cz>
2025-12-02 13:23:26 +0000

    Refactor polymerization logic and add interactive number guessing game; update README with new exercises

--- priklady-z-hodin/2025-2026/aoc_21_14/main.py
+++ priklady-z-hodin/2025-2026/aoc_21_14/main.py
@@ -1,30 +1,58 @@
 from os.path import join, dirname, realpath
+from functools import lru_cache
 
 
-def load_file(cesta: str = join(dirname(realpath(__file__)), "data.txt")) -> str:
+def load_file(cesta: str = join(dirname(realpath(__file__)), "test.txt")) -> str:
     with open(cesta, "r", encoding="utf-8") as f:
         return f.read()
 
 
-def prepare_file(soubor: str) -> tuple[str, dict]:
-    vystup = soubor.split('\n\n')
-    vystup[1] = {
-        radek.split('->')[0].strip(): radek.split('->')[1].strip()
-        for radek in vystup[1].split('\n')
-        }
-    return tuple(vystup)
+vystup = load_file().split('\n\n')
+vystup[1] = {
+    radek.split('->')[0].strip(): radek.split('->')[1].strip()
+    for radek in vystup[1].split('\n')
+}
+vychozi_polymer = vystup[0]
+pravidla = vystup[1]
 
-def polymerizace(pravidla:dict, polymer:str, pocet_cyklu:int)->str:
-    for _ in range(pocet_cyklu):
-        rozdeleny_polymer = [polymer[i]+pravidla[polymer[i]+polymer[i+1]]+polymer[i+1] for i in range(len(polymer)-1)] 
-        polymer = rozdeleny_polymer[0]+"".join([prvek[1:] for prvek in rozdeleny_polymer[1:]])
-    elementy = set(polymer)
-    pocty = [polymer.count(element) for element in elementy]
-    return max(pocty)-min(pocty)
+@lru_cache(maxsize=100)
+def polymerizace_recursive(polymer: str, pocet_cyklu) -> dict:
+    if len(polymer) > 2:
+        vysledky = [
+            {k: (v-1 if 
+                 (k == polymer[i+1] and i<len(polymer)-2) 
+                 else v) for k, v 
+             in polymerizace_recursive(polymer[i] + polymer[i+1], pocet_cyklu).items()
+             } 
+                    for i in range(len(polymer)-1)]
+        keys = set()
+        for key in vysledky:
+            keys.update(key.keys())
+        vysledek = {k:0 for k in keys}
+        for d in vysledky:
+            for k,v in d.items():
+                vysledek[k]+=v
+        return vysledek
+
+    if pocet_cyklu > 0:
+        rozsirujici_prvek = pravidla[polymer]
+        polymer = polymer[0]+rozsirujici_prvek+polymer[1]
+        vysledek_1 = polymerizace_recursive(polymer[:2], pocet_cyklu-1)
+        vysledek_2 = polymerizace_recursive(polymer[1:], pocet_cyklu-1)
+        vysledek = {k: vysledek_1.get(k, 0) + vysledek_2.get(k, 0)
+                    for k in set(vysledek_1) | set(vysledek_2)}
+        vysledek[rozsirujici_prvek] -= 1
+        return vysledek
+
+    else:
+        return {c: polymer.count(c) for c in set(polymer)}
+
 
 def main():
-    vychozi_polymer, pravidla = prepare_file(load_file())
-    print(polymerizace(pravidla, vychozi_polymer, 40))
-    
+    vysledky = polymerizace_recursive(vychozi_polymer, 40)
+    serazene_vysledky =sorted(vysledky.values())
+    print(serazene_vysledky[-1]-serazene_vysledky[0])
+
+
 if __name__ == "__main__":
     main()
